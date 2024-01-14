32a187b336515671c38434b0fa6a97d81fada63a
Arya Patel <97847987+Arya-P05@users.noreply.github.com>
2024-01-13 22:41:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-// Create and initialise GSAP timeline
 const tl = gsap.timeline({ defaults: { duration: 0.75, ease: "power3.out" } });
 
-// Hero Section Animation
 tl.fromTo(
 	`.hero-img`,
 	{ scale: 1.3, borderRadius: 0 },
@@ -21,23 +19,15 @@ tl.fromTo(
 	.fromTo(`.cta-5`, { y: "100%", opacity: 0 }, { y: 0, opacity: 1 }, "<20%")
 	.fromTo(`.cta-btn`, { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, "<50%");
 
-// Split Text
 const logo = document.querySelector(`.logo`);
 const letters = logo.textContent.split("");
 
-// Get rid of the original text
 logo.textContent = "";
 
-// Create the text anew, with a span surrounding each character
 letters.forEach((letter) => {
 	logo.innerHTML += `<span class="letter">${letter}</span>`;
 });
 
-/*
-    Set the display of all elements with a class of "letter" to 'inline-block'
-    That will allow us to animate the x and y values of the span elements,
-    which are originally 'inline' elements, that do not have a height property
-*/
 gsap.set(`.letter`, { display: `inline-block` });
 
 gsap.fromTo(
@@ -49,7 +39,7 @@ gsap.fromTo(
 document.addEventListener('DOMContentLoaded', function () {
 	setTimeout(function () {
 	  document.querySelector('.pic-claim').style.opacity = '1';
-	}, 3000); // Adjust the delay in milliseconds (3 seconds in this example)
+	}, 3000);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -64,10 +54,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function openResume() {
-	// Change the file path to your actual PDF file location
 	var resumeUrl = 'assets/FINAL RESUME.pdf';
-  
-	// Open the PDF in a new tab
 	window.open(resumeUrl, '_blank');
 }
   
