eacce7fa4a166962b14ff8203243499c37c3eb91
Arya <jackrack040@gmail.com>
2024-12-23 21:04:20 -0500

    complete rebranding

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import Hero from "@/components/Hero";
-
 export default function Home() {
-  return (
-    <main className="bg-black">
-      <Hero />
-    </main>
-  );
+  return <></>;
 }
