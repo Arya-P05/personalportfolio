27a4ff63fbe7a33283bb7cdb05ae74ce410796b6
Arya <jackrack040@gmail.com>
2024-12-26 17:53:27 -0500

    signatures and new bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import "./globals.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <h1>hi</h1>
+      <div className="logo-container">
+        <Image
+          src="/signature.png"
+          alt="Logo"
+          width={173.875}
+          height={77.375}
+          priority
+        />
+      </div>
+      <h1>Arya Patel &</h1>
     </>
   );
 }
