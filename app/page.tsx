f6add7dae50b0b642a72ef51e28dba687fabe7c2
Arya <jackrack040@gmail.com>
2024-12-26 20:28:21 -0500

    font and svg signature

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="logo-container">
+      <div className="logo">
         <Image
-          src="/signature.png"
+          src="/signature.svg"
           alt="Logo"
           width={173.875}
           height={77.375}
           priority
         />
       </div>
+      <h1>My</h1>
+      <h1>Contact</h1>
       <h1>Arya Patel</h1>
+      <h2>test</h2>
     </>
   );
 }
